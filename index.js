0fff38625b491d6fe624b945b480527c708db049
Collins Chirchir <collinskchirchir@gmail.com>
2022-11-26 13:38:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ['Collins', 'Evans', 'Tony', 'Dennis', 'Clifford'];
+
+function writeCards(arry, eventName){
+   const msgArry = []
+   for(let name = 0; name < arry.length; name++)
+   {  
+      let greeting = (`Thank you, ${arry[name]}, for the wonderful ${eventName} gift!`);
+      msgArry.push(greeting);
+   }
+   return msgArry;
+}
+
+function countDown(numb)
+{
+   let intial = numb;
+   while(numb >= 0)
+   {
+      console.log(numb);
+      numb--;
+   }
+}
+countDown(10)
